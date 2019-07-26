009299e2271b7a3723b27691e3fa62821afa9b57
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-26 08:15:59 +0100

    add users model

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,26 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const { authenticate } = require('../auth/authenticate');
-
+const { authenticate } = require("../auth/authenticate");
+const Users = require('../models/users')
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
   // implement user registration
+  const { username, password } = req.body;
+
+  console.log(username, password);
+  if (!username && !password) {
+    return res
+      .status(400)
+      .json({
+        status: "error",
+        message: "Username and Password fields are required"
+      });
+  }
 }
 
 function login(req, res) {
@@ -18,15 +29,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
